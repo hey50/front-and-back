aed620b1782247fdad688107c6cb5b559bc7bf31
heba mo <Hebq@ndbs>
2024-03-11 11:44:18 +0200

    first commit

---
+++ client/src/firebase.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: 'mern-estate.firebaseapp.com',
+  projectId: 'mern-estate',
+  storageBucket: 'mern-estate.appspot.com',
+  messagingSenderId: '1078482850952',
+  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+};
+
+
+export const app = initializeApp(firebaseConfig);
